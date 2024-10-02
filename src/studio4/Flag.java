b2c277e248794d2151fd5715b2e61fa64be3b3c7
noahcalabrese <noahcalabrese@localhost>
2024-10-02 16:34:04 -0500

    done, done, done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,21 @@
 package studio4;
 
 import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
+import java.util.Scanner;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setCanvasSize(600, 800);
+        StdDraw.setXscale(0, 6);
+        StdDraw.setYscale(0, 6);
+        
+		StdDraw.clear();
+		StdDraw.setPenColor(100, 200, 175);
+		StdDraw.filledRectangle(5, 2, 2, 5);
+		StdDraw.setPenColor(175, 75, 175);
+		StdDraw.filledCircle(2,3,2);
 	}
 }
